86712a0ffc2bd467aec899509703b0cf2e947834
student <student@gnu.ac.in>
2023-07-19 08:53:37 +0530

    commit

---
+++ src/main/kotlin/Practical-1.4.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter number")
+    val a = readLine()!!.toInt()
+    val s = if(a%2==0)
+        "$a is even number"
+    else
+        "$a is odd number"
+
+    println(s)
+
+}
